7566dec51472499d2dc8ab5f83c8134f032b1814
danielsport156 <daniel.estepa13@gmail.com>
2020-09-18 22:43:42 +0000

    updt

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<studio.h>
+/**
+*main - Greeting the World
+*
+*Return: 0 is no error
+*/
+int main(void)
+{
+puts("\"Programming is like building a multi level of languages")
+return (0);
+}
