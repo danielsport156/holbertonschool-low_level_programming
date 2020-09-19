336b38333475adc5e28e4b9e7fea32482fc4159d
danielsport156 <daniel.estepa13@gmail.com>
2020-09-19 04:15:16 +0000

    updt

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,22 @@
 #include<stdio.h>
 /**
-
-*main - Entry point
-
-*
-
-* Return: Always 0 (Success)
-
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-  char charType;
-  int intType;
-  long int longintType;
-  long long int longlongintType;
-  float floatType;
+char charType;
+int intType;
+long int longintType;
+long long int longlongintType;
+float floatType;
 
-  printf("Size of a char: %d byte(s)\n", sizeof(charType));
-  printf("Size of an int: %d byte(s)\n", sizeof(intType));
-  printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
-  printf("Size of a long long int: %d byte(s)\n", sizeof(longlongintType));
-  printf("Size of a float: %d byte(s)\n", sizeof(floatType));
-  return (0);
+printf("Size of a char: %d byte(s)\n", sizeof(charType));
+printf("Size of an int: %d byte(s)\n", sizeof(intType));
+printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
+printf("Size of a long long int: %d byte(s)\n", sizeof(longlongintType));
+printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+return (0);
 }
