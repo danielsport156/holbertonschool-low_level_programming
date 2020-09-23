85d1d62b4691f4e4a25dfb4b62c8e286ae5be3d9
danielsport156 <daniel.estepa13@gmail.com>
2020-09-23 17:16:08 +0000

    updt

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * main - print Holberton
+ *
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	_putchar ('H');
+	_putchar ('o');
+	_putchar ('l');
+	_putchar ('b');
+	_putchar ('e');
+	_putchar ('r');
+	_putchar ('t');
+	_putchar ('o');
+	_putchar ('n');
+	return (0);
+}
