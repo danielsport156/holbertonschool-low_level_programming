6a751ad2976ab0fed1d0739d3acef4b5f6efd6ce
danielsport156 <daniel.estepa13@gmail.com>
2020-09-24 04:18:49 +0000

    updt

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 	char ch;
 	int c = 0;
 	int max = 10;
+
 	for ( ; c < max; ++c)
 	{
-		for ( ch = 'a'; ch <= 'z'; ++ch)
+		for (ch = 'a'; ch <= 'z'; ++ch)
 			_putchar (ch);
 		_putchar ('\n');
 	}
